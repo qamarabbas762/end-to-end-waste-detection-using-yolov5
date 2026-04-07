fe0dbd54b2a2d4b81b32596efd898738ef155b70
qamarabbas762 <99537800+qamarabbas762@users.noreply.github.com>
2026-04-08 03:58:01 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,44 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(levelname)s - %(message)s'  )
+
+project_name = "wasteDetection"
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "data/.gitkeep",
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/constants/training_pipeline/__init__.py",
+    f"{project_name}/constants/application.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "templates/index.html",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py"
+]
+
+for file in list_of_files:
+    file_path = Path(file)
+    file_dir, file_name = os.path.split(file_path)
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok = True)
+        logging.info(f"Creating directory: {file_dir} for file: {file_name}")
+    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        with open(file_path, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {file_path}")
+    else:
+        logging.info(f"File already exists and is not empty: {file_path}")
